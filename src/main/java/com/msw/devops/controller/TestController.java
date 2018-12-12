8ade3ec722426a3f1eb448a0ae071e0fee376999
mashuangwei-homePc <mashuangwei@aliyun.com>
2018-12-12 23:46:00 +0800

    新增elk整合

--- src/main/java/com/msw/devops/controller/TestController.java
+++ src/main/java/com/msw/devops/controller/TestController.java
@@ -4,6 +4,9 @@ import com.msw.devops.annotation.Log;
 import com.msw.devops.entity.User;
 import com.msw.devops.exception.Result;
 import com.msw.devops.util.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +23,13 @@ import javax.validation.Valid;
 @RequestMapping("/test")
 public class TestController {
 
+    private final static Logger logger = LoggerFactory.getLogger( TestController.class );
+
     @GetMapping("/test")
     public Result getUser(){
+        logger.info("info= {}", "test info");
+        logger.error("error= {}", "test error");
+        logger.debug("debug= {}", "test debug");
         return ResultUtil.success(200);
     }
 
